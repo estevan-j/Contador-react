0aee171a005f13c4ac86309fadb63e4ac884beda
estevan-j <esteban.bajana@epn.edu.ec>
2023-09-30 02:30:09 -0500

    final

--- src/App.js
+++ src/App.js
@@ -1,14 +1,41 @@
+import { useState } from 'react';
 import './App.css';
+import Boton from './Components/Boton';
+import Contador from './Components/Contador';
 import freeCodeCampLogo from './img/freecodecamp-logo.png';
+
 function App() {
+  
+  const [numClicks, setNumClicks] = useState(0);
+
+  const manejarClic = () =>{
+    setNumClicks(numClicks+1);
+  }
+  const reiniciarContador = () => {
+    setNumClicks(0);
+  }
+
   return (
     <div className="App">
-      <div className='freedocecamp-logo-contenedor'>
+      <div className='freecodecamp-logo-contenedor'>
         <img
-          className='logo'
+          className='freecodecamp-logo'
           src={freeCodeCampLogo}
           alt='logo de freeCodeCamp'
          />
+      </div>
+      <div className='contenedor-principal'>
+        <Contador numClicks={numClicks} />
+        <Boton
+          texto={'click'}
+          esBotonClick={true}
+          manejarClick={manejarClic}
+        />
+        <Boton
+          texto={'reiniciar'}
+          esBotonClick={false}
+          manejarClick={reiniciarContador}
+        />
       </div>
     </div>
   );
---
+++ src/Components/Boton.js
@@ -0,0 +1,15 @@
+import React from "react";
+import '../styles/Boton.css'
+
+function Boton({texto, esBotonClick, manejarClick}) {
+  return( 
+    <button 
+    className={ esBotonClick ? "boton-click": "boton-reiniciar" }
+    onClick={()=> manejarClick()}>
+        {texto}
+    </button>
+    );
+}
+
+
+export default Boton;
